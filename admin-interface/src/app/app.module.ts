1b98522f474fd856180d44f8d968dab5389d06ad
Pavithra Sureshkumar <pasuit00@hs-esslingen.de>
2021-11-12 04:47:26 +0100

    Add routing and allgemein-interface #26

--- admin-interface/src/app/app.module.ts
+++ admin-interface/src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule} from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import { AppComponent } from './app.component';
+import { AllgemeinInterfaceComponent } from './allgemein-interface/allgemein-interface.component';
+import { Router, RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllgemeinInterfaceComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule,
     MatMenuModule,
-    MatIconModule,
-
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
