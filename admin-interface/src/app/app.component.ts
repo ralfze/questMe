e12764464e7c6f54fda31c85137adf9dbd4921b5
Pavithra Sureshkumar <pasuit00@hs-esslingen.de>
2021-11-12 04:22:31 +0100

    Completed hamburger menu #26

--- admin-interface/src/app/app.component.ts
+++ admin-interface/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, ViewChild } from '@angular/core';
-import {MatMenuModule} from '@angular/material/menu';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
   title = 'admin-interface';
 }
--- admin-interface/src/app/app.module.ts
+++ admin-interface/src/app/app.module.ts
@@ -4,8 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule} from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon'
-
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -15,7 +14,8 @@ import { MatIconModule } from '@angular/material/icon'
     BrowserModule,
     BrowserAnimationsModule,
     MatMenuModule,
-    MatIconModule
+    MatIconModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
